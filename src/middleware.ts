8868c8611cb750ce06d1a38525cd4a147ea545b2
eric.shady <darkercookies@gmail.com>
2023-03-02 06:25:41 +0800

    fix: change the middleware for loggin routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,18 @@
-export { default } from "next-auth/middleware"
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const sessionToken = request.cookies.get('next-auth.session-token')?.value
+  if(!sessionToken){
+    const { pathname } = request.nextUrl
+    return NextResponse.rewrite(new URL(`/api/auth/signin?callbackUrl=${pathname}`, request.url))
+  }
+
+  const response = NextResponse.next()
+  return response
+}
 
 // See "Matching Paths" below to learn more
 export const config = {
- matcher: ['/todo/:path*', "/posts/create", "/posts/drafts", "/posts/p/:path*"]
-}
+  matcher: ['/todo/:path*', "/posts/create", "/posts/drafts", "/posts/p/:path*"]
+ }
